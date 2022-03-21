beebe7fc2e401cd6ccd37bb5da22406ab736c677
Realdens <denis.eleyehou@hotmail.fr>
2022-03-21 17:51:53 +0100

    first commit

---
+++ fil_rouge2/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="bg-fuchsia-500">Ceci est un titre h1</h1>
+    <TheWelcome />
+  </main>
+</template>
